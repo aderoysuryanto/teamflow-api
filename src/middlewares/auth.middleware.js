a04568b1d3b5319c6a921c84d6b7c3f347dbd4da
aderoysuryanto <aderoysuryanto@gmail.com>
2026-03-12 10:24:39 +0700

    feat: add auth middleware

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,28 @@
+import jwt from "jsonwebtoken"
+
+export function authMiddleware(req, res, next) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({
+      message: "Unauthorized"
+    })
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const payload = jwt.verify(
+      token,
+      process.env.JWT_ACCESS_SECRET
+    );
+
+    req.user = payload;
+
+    next();
+  } catch (err) {
+    return res.status(401).json({
+      message: "Invalid token"
+    });
+  }
+}
