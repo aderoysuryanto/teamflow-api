ed013809ab8fcd2476e3c69c72a2c22a9352864f
aderoysuryanto <aderoysuryanto@gmail.com>
2026-04-07 09:38:20 +0700

    fix: validation middleware

--- src/middlewares/validate.middleware.js
+++ src/middlewares/validate.middleware.js
@@ -1,13 +1,15 @@
 export function validate(schema) {
   return (req, res, next) => {
-    try {
-      schema.parse(req.body);
-      next();
-    } catch (err) {
+    const result = schema.safeParse(req.body);
+
+    if (!result.success) {
       res.status(400).json({
         message: "Validation error",
-        errors: err.errors
+        errors: result.error.flatten().fieldErrors
       });
     }
+
+    req.body = result.data;
+    next();
   };
 }
